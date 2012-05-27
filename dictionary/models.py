422a6b9686cd41284c6cd10ba7d0273be27575f8
Gizem Gür <grgizem@gmail.com>
2012-05-27 21:22:10 +0300

    Changes dictionary/model.py : created models

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    owner = models.ForeignKey(User)
+    def __unicode__(self):
+	return self.name
+
+class Entry(models.Model):
+    content = models.TextField(max_length=5000)
+    author = models.ForeignKey(User)
+    date = models.DateField(auto_now=True)
+    belong = models.ForeignKey(Item)
+    class Meta:
+	verbose_name_plural = "Entries"
+    def __unicode__(self):
+        return self.content
+
+
