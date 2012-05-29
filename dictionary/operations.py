c0453307e460055487fa0d30e8a020c00d1ee918
Gizem Gür <grgizem@gmail.com>
2012-05-29 20:54:54 +0300

    changed dictionary/operation.py

--- dictionary/operations.py
+++ dictionary/operations.py
@@ -28,6 +28,6 @@ def shorten_url(long_url):
      username = settings['BITLY_USERNAME']
      password = settings['BITLY_PASSWORD']
      bitly_url = "http://api.bit.ly/v3/shorten?login={0}&apiKey={1}&longUrl={2}&format=txt"
-     req_url = urlencode(bitly_url.format(username, password, long_url)
-     short_url = urlopen(req_url)
-     return short_url.read()
+     req_url = urlencode(bitly_url.format(username, password, long_url))
+     short_url = urlopen(req_url).read()
+     return short_url
