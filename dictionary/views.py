e445634e0a2ba8b6d16f26d4411724297d876545
Gizem Gür <grgizem@gmail.com>
2012-05-29 11:58:09 +0300

    changed templates/index.html,user.html dictionary/views.html : entries of items

--- dictionary/views.py
+++ dictionary/views.py
@@ -26,6 +26,7 @@ def home(request):
     i = get_object_or_404(Item, id=item_id)
     ctx = {
             'item': i,
+            'entries': i.entries.all()
             'hot_items': hot_items()
     }
     if request.user.is_authenticated():
