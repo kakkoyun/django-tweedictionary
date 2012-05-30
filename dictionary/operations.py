5ba616c8f6cc5b0efdb2048c9541003a609ebc08
Kemal Akkoyun <kakkoyun@gmail.com>
2012-05-30 17:10:40 +0300

    operations.py: shorten_url and bit.ly related fucntions corrected.

--- dictionary/operations.py
+++ dictionary/operations.py
@@ -7,30 +7,27 @@ import oauth2 as oauth
 import twitter
 
 def send(request,entry_id):
-	twitter_user = request.user.social_auth.get(provider='twitter')
+    twitter_user = request.user.social_auth.get(provider='twitter')
 
-	if not twitter_user.tokens:
-	    return
+    if not twitter_user.tokens:
+        return
 
     access_token = twitter_user.tokens['oauth_token']
-	access_token_secret = twitter_user.tokens['oauth_token_secret']
+    access_token_secret = twitter_user.tokens['oauth_token_secret']
 
-	token = oauth.Token(access_token,access_token_secret)
-	consumer_key = settings.TWITTER_CONSUMER_KEY
-	consumer_secret = settings.TWITTER_CONSUMER_SECRET
-	consumer = oauth.Consumer(consumer_key,consumer_secret)
-	client = oauth.Client(consumer,token)
+    #token = oauth.Token(access_token,access_token_secret)
+    consumer_key = settings.TWITTER_CONSUMER_KEY
+    consumer_secret = settings.TWITTER_CONSUMER_SECRET
+    #consumer = oauth.Consumer(consumer_key,consumer_secret)
+    #client = oauth.Client(consumer,token)
 
-	api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret, 
+    api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret, 
           access_token_key=access_token,
           access_token_secret=access_token_secret)
 
+    data = (get_object_or_404(Entry, id=entry_id).content)[:122]+"..."+shorten_url("http://www.tweedictionary.com/entry/%s" %entry_id)
     api.PostUpdate(get_object_or_404(Entry, id=entry_id).content)
-
-	#data = (get_object_or_404(Entry, id=entry_id).content)[:115]+"..."+shorten_url("http://www.tweedictionary.com/entry/%s" %entry_id)
-#	data = get_object_or_404(Entry, id=entry_id).content
-#	request_uri = 'https://api.twitter.com/1/statuses/update.json'
-#	resp, content = client.request(request_uri, 'POST', data)
+    #data = get_object_or_404(Entry, id=entry_id).content
 
 def shorten_url(long_url):
     username = settings.BITLY_USERNAME
