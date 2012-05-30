96e0d287a21e968d5fa530d83a1176d11a197905
Kemal Akkoyun <kakkoyun@gmail.com>
2012-05-30 16:08:52 +0300

    Testing .gitignore

--- tweedictionary/settings.py
+++ tweedictionary/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tweedictionary',                      # Or path to database file if using sqlite3.
-        'USER': 'dictiondb',                      # Not used with sqlite3.
-        'PASSWORD': 'repltwit15',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'tweedictionary.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
